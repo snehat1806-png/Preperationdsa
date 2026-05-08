f15c8d969a7406227644756766178555fde67193
Sneha Tiwari <snehat1806@gmail.com>
2026-05-08 11:11:05 +0530

    Basic questions backlogs today done..

---
+++ p10.java
@@ -0,0 +1,12 @@
+public class p10 {
+    //Calculate Commission Percentage
+    public static void main(String[] args) {
+        double sales=5000;
+        double commissionRate=0.1; //10% commission
+        double commission=sales*commissionRate;
+        //Commission is calculated by multiplying the sales amount with the commission rate.
+        //In this case, the commission will be 5000 * 0.1 = 500.
+        //The commission is then printed to the console.
+        System.out.println("Commission is: "+commission);
+    }
+}
---
+++ p8.java
@@ -0,0 +1,9 @@
+public class p8 {
+    //Calculate Discount Of Product
+    public static void main(String[] args) {
+        double price=1000;
+        double discount=10;
+        double discountedPrice=price-(price*discount/100);
+        System.out.println("Discounted Price is: "+discountedPrice);
+    }
+}
