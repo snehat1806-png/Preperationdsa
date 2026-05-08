6fe829866c19d0c13da32a17a0e5c1a3253ac2f2
Sneha Tiwari <snehat1806@gmail.com>
2026-05-08 10:47:58 +0530

    Unique sum problem

---
+++ Summ.java
@@ -0,0 +1,13 @@
+public class Summ {
+    //Sum of digits of a number
+    public static void main(String[] args) {
+        int num=23456;
+        int sum=0;
+        while(num>0){
+            int digit=num%10;
+            sum+=digit;
+            num=num/10;
+        }
+        System.out.println("Sum of digits is: "+sum);
+}
+}
