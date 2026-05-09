0a45b0e7d4a1eae51e9ebcbceba1168b1d186af2
Sneha Tiwari <snehat1806@gmail.com>
2026-05-09 22:08:07 +0530

    Add files via upload

---
+++ leetcode.java
@@ -0,0 +1,17 @@
+public class leetcode {
+//https://leetcode.com/problems/build-array-from-permutation/submissions/1999064351/
+    public static void main(String[] args) {
+        int[] nums={0,2,1,5,3,4};
+        int[] ans=buildArray(nums);
+        for(int i:ans){
+            System.out.print(i+" ");
+        }
+    }
+    static int[] buildArray(int[] nums) {
+        int[] ans=new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            ans[i]=nums[nums[i]];
+        }
+        return ans;
+    }    
+}
