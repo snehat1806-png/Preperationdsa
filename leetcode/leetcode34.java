fa29244903e46214a0a532eeba4d7afc07ed2062
snehat1806-png <snehat1806@gmail.com>
2026-06-10 17:33:58 +0530

    Update leetcode solutions

--- leetcode/leetcode34.java
+++ leetcode/leetcode34.java
@@ -1,1 +1,31 @@
 //https://leetcode.com/problems/valid-perfect-square/
+public class leetcode34 {
+    public static void main(String[] args) {
+        int num = 16;
+        System.out.println(isPerfectSquare(num));
+    }
+
+    static boolean isPerfectSquare(int num) {
+        if (num < 2) {
+            return true;
+        }
+
+        long low = 1;
+        long high = num / 2;
+
+        while (low <= high) {
+            long mid = low + (high - low) / 2;
+            long square = mid * mid;
+
+            if (square == num) {
+                return true;
+            } else if (square < num) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return false;
+    }
+}
--- leetcode/leetcode35.java
+++ leetcode/leetcode35.java
@@ -1,3 +1,28 @@
 public class leetcode35 {
 //https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/description/   
+    public static void main(String[] args) {
+        int[] numbers = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = twoSum(numbers, target);
+        System.out.println("Indices: " + result[0] + ", " + result[1]);
+    }
+
+    static int[] twoSum(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length - 1;
+
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+
+            if (sum == target) {
+                return new int[]{left + 1, right + 1}; // Return 1-based indices
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return new int[]{-1, -1}; // Return -1 if no solution is found
+    }
 }
