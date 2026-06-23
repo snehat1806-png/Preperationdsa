45f56196efe08c1c695bc9cdf73d67ad9c13ec49
snehat1806-png <snehat1806@gmail.com>
2026-06-23 22:12:46 +0530

    Update leetcode48 solution

--- leetcode/leetcode48.java
+++ leetcode/leetcode48.java
@@ -1,3 +1,17 @@
 public class leetcode48 {
-//https://leetcode.com/problems/check-if-n-and-its-double-exist/description/    
+//https://leetcode.com/problems/check-if-n-and-its-double-exist/description/   
+    public static void main(String[] args) {
+        int[] arr = {10,2,5,3};
+        System.out.println(checkIfExist(arr));
+    }
+    public static boolean checkIfExist(int[] arr){
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr.length;j++){
+                if(i!=j && arr[i]==2*arr[j]){
+                    return true;
+                }
+            }
+        }
+        return false;
+    } 
 }
