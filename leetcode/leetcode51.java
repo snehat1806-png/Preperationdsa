43c2c97f92da550aed1ec9ace3ec5503674d2f62
snehat1806-png <snehat1806@gmail.com>
2026-06-25 22:10:50 +0530

    Commit from VS Code

---
+++ leetcode/leetcode51.java
@@ -0,0 +1,21 @@
+//https://leetcode.com/problems/single-element-in-a-sorted-array/submissions/2045981157/
+public class leetcode51 {
+    public static void main(String[] args) {
+        int[] arr = {1,1,2,3,3,4,4,8,8};
+        System.out.println(singleNonDuplicate(arr));
+    }
+    public static int singleNonDuplicate(int[] arr){
+        int left = 0;
+        int right = arr.length-1;
+        while(left<right){
+            int mid = left + (right-left)/2;
+            if(mid%2==1) mid--;
+            if(arr[mid]==arr[mid+1]){
+                left = mid+2;
+            }else{
+                right = mid;
+            }
+        }
+        return arr[left];
+    }   
+}
