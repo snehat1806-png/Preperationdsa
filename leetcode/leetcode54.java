399100f18b3494c5a3f89616d111db9a13681cd0
snehat1806-png <snehat1806@gmail.com>
2026-06-29 16:23:01 +0530

    Add solution for leetcode 54

---
+++ leetcode/leetcode54.java
@@ -0,0 +1,30 @@
+//https://leetcode.com/problems/valid-palindrome-ii/submissions/2049947419/
+public class leetcode54 {
+    public static void main(String[] args) {
+        String s = "abca";
+        System.out.println(validPalindrome(s));
+    }
+    public static boolean validPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    private static boolean isPalindrome(String s, int left, int right) {
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
